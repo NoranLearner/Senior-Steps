4c3ea307ebcb8c2462f5bf281000d57865b05dc1
NoranLearner <noranmostafa843@gmail.com>
2023-09-13 10:40:42 +0300

    Lecture 1

---
+++ first/document1.php
@@ -0,0 +1,42 @@
+<?php
+
+$firstName = 'Ahmed';
+
+$secondName = 'Mohsen';
+
+echo "Hello $firstName $secondName";
+
+echo "<br>";
+
+print "Hello $firstName $secondName";
+
+echo "<br>";
+
+echo 'My Full Name is ' , $firstName , ' ' , $secondName;
+
+echo "<br>";
+
+// Error
+// print 'My Full Name is ' , $firstName , ' ' , $secondName;
+
+echo "<br>";
+
+echo 'My Full Name is ' . $firstName . ' ' . $secondName;
+
+echo "<br>";
+
+print 'My Full Name is ' . $firstName . ' ' . $secondName;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome from document 1 file</h1>
+</body>
+</html>
